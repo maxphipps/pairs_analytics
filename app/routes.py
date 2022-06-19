5cff6f57ff0cab34bde5368cf0c7d77d284967b4
Max Phipps <maxphipps4@gmail.com>
2022-06-19 13:34:39 +0100

    Embed bokeh server as a library

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,6 @@
 from flask import render_template
+from bokeh.embed import server_document
 from app import app
-from bokeh.embed import components
-
-from .plots import Dashboard
-
 
 @app.after_request
 def add_header(r):
@@ -17,26 +14,7 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
-
-@app.route('/')
-@app.route('/index')
-def index():
-    """
-    Main dashboard page
-    :return:
-    """
-    from bokeh.sampledata.stocks import AAPL, GOOG, IBM, MSFT
-
-    plot_dashboard_obj = Dashboard(ticker_tuples=(#('AAPL', AAPL),
-                                                   #('GOOG',GOOG),
-                                                   ('IBM',IBM),
-                                                   ('MSFT',MSFT),
-                                                   )).get_plot()
-
-    _script, _div = components(plot_dashboard_obj)
-    plot_dashboard = {'script': _script, 'div': _div}
-
-    return render_template('index.html', title='plot',
-                           # prices_plot=prices_plot,
-                           # plot_ma=plot_ma,
-                           plot_dashboard=plot_dashboard)
+@app.route('/', methods=['GET'])
+def bkapp_page():
+    script = server_document('http://localhost:5006/bkapp')
+    return render_template("index.html", script=script, template="Flask")
--- pairs_analytics.py
+++ pairs_analytics.py
@@ -1,13 +1,53 @@
+from threading import Thread
+from tornado.ioloop import IOLoop
 
 from app import app
+from bokeh.server.server import Server
+from bokeh.themes import Theme
+
+from bokeh.sampledata.stocks import AAPL, GOOG, IBM, MSFT
+
+from app.plots import Dashboard
+
 
 # To deploy on Heroku:
 # FLASK + BOKEH + HEROKU =>
 #   https://pjandir.github.io/Bokeh-Heroku-Tutorial/
+
+
+def bkapp(doc):
+    plot_dashboard_obj = Dashboard(ticker_tuples=(#('AAPL', AAPL),
+                                                   #('GOOG', GOOG),
+                                                   ('IBM', IBM),
+                                                   ('MSFT', MSFT),
+                                                   )).get_plot()
+
+    doc.add_root(plot_dashboard_obj)
+    doc.theme = Theme(filename="theme.yaml")
+
+
+def bk_worker():
+    """
+    Main dashboard page
+    :return:
+    """
+    # Can't pass num_procs > 1 in this configuration. If you need to run multiple
+    # processes, see e.g. flask_gunicorn_embed.py
+    server = Server({'/bkapp': bkapp}, io_loop=IOLoop(), allow_websocket_origin=["localhost:8000", "127.0.0.1:8000"])
+    server.start()
+    server.io_loop.start()
+
+
+Thread(target=bk_worker).start()
+
 
 # https://blog.miguelgrinberg.com/post/setting-up-a-flask-application-in-pycharm
 # Use different version of run in order to enable the debugger
 # "More specifically, this is a bug that affects Flask applications that are started with the flask run command,
 # but works well with the old app.run() method of running the application."
 if __name__ == '__main__':
-    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
+    print('Opening single process Flask app with embedded Bokeh application on http://localhost:8000/')
+    print()
+    print('Multiple connections may block the Bokeh app in this configuration!')
+    print('See "flask_gunicorn_embed.py" for one way to run multi-process')
+    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True, port=8000)
